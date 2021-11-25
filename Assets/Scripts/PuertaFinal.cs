ef5eee8f21b4e4b3dadb23255943c544b67e3176
mdemartino26 <mdmyec@gmail.com>
2021-11-24 23:22:29 -0300

    :)

---
+++ Assets/Scripts/PuertaFinal.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaFinal : MonoBehaviour
+{
+  public WinScreen winScreen;
+    public void Win()
+    {
+        winScreen.Setup();
+    }
+}
---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+     public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void RestartButton(){
+        SceneManager.LoadScene("SampleScene");
+    }
+}
